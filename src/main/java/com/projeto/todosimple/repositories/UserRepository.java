fc1052a057f428cfbbaa8f5a1948134fde8e0963
enzoboto1 <enzoboto1.0@gmail.com>
2024-03-09 18:57:15 -0300

    Create UserService

--- src/main/java/com/projeto/todosimple/repositories/UserRepository.java
+++ src/main/java/com/projeto/todosimple/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.projeto.todosimple.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.projeto.todosimple.models.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-    
+
 }
